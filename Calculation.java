3e3f379d98f8fcb5903094e28a2b22081ecaafe1
snehaltitme <snehaltitme135@gmail.com>
2025-10-09 15:08:21 +0530

    Basic java program - calculation
    Basic Java program – calculation using arithmetic operators

---
+++ Calculation.java
@@ -0,0 +1,21 @@
+package understandingdatatypes;
+
+public class Calculation {
+	
+	// First Assignment
+
+	public static void main(String[] args)
+	{
+		
+		int Maths=90, Science=80, English=70 ;
+		int Total=(Maths+Science+English);
+		double Average= (Total/3);
+		
+		System.out.println("Total Marks= " +Total);
+		
+		System.out.println("Average Marks= "+ Average);
+		
+		
+	}
+
+}
