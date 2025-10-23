8060ac79c1923da1d78b176e0198920a8575b844
snehaltitme <snehaltitme135@gmail.com>
2025-10-23 17:15:36 +0530

    7 table

---
+++ Table.java
@@ -0,0 +1,19 @@
+package loopingprograms;
+
+public class Table {
+
+	public static void main(String[] args) {
+
+		
+	int i;
+	int sum=0;
+	
+	for(i=1 ; i<=10; i++) {
+		
+		sum=i*7;
+		System.out.println(sum);
+	}
+	
+	}
+
+}
